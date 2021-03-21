99bc6383f47fed85835c756797b48fb089a1b3f3
amolina19 <alejandro.molina.daw@gmail.com>
2021-03-21 22:02:43 +0100

    Update

--- backend/API/app/api/models/users.js
+++ backend/API/app/api/models/users.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 // Cargamos el módulo de bcrypt
 const bcrypt = require('bcrypt');
 // Definimos el factor de costo, el cual controla cuánto tiempo se necesita para calcular un solo hash de BCrypt. Cuanto mayor sea el factor de costo, más rondas de hash se realizan. Cuanto más tiempo sea necesario, más difícil será romper el hash con fuerza bruta.
+var encrypt = require('mongoose-encryption');
+//Para encriptar el objeto en la base de datos
+const dotenv = require('dotenv');
+dotenv.config();
+//Variables de entorno donde estaran las claves
 const saltRounds = 10;
 //Definimos los esquemas
 const Schema = mongoose.Schema;
