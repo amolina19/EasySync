5c10a4be404ff98905a717760dcb8b7969069dcc
amolina19 <alejandro.molina.daw@gmail.com>
2021-03-21 20:55:04 +0100

    Update domain

--- backend/API/app/api/controllers/users.js
+++ backend/API/app/api/controllers/users.js
@@ -2,46 +2,67 @@ const userModel = require('../models/users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+function checkUri(request){
+    if(request.includes('api')){
+        return true;
+    }
+    return false;
+}
+
 module.exports = {
     create: function(req, res, next){
 
-        userModel.find({$and:[{username:req.body.username},{email:req.body.email}]}, function(err,result){
-            if(result.length === 0){
-                userModel.create({username: req.body.username, email: req.body.email, password: req.body.password,created_at: new Date()}, function(err){
-                    if(err){
-                        next(err);
-                    }else{
-                        res.json({status:"ok",message:"User created"})
-                    }
-                });
-            }else{
-                res.json({status:"error", message: "User exists"});
-            }
-        });
-        
+        if(checkUri(req.headers.host)){
+            userModel.find({$and:[{username:req.body.username},{email:req.body.email}]}, function(err,result){
+                if(result.length === 0){
+                    userModel.create({username: req.body.username, email: req.body.email, password: req.body.password,created_at: new Date()}, function(err){
+                        if(err){
+                            next(err);
+                        }else{
+                            res.json({status:"ok",message:"User created"})
+                        }
+                    });
+                }else{
+                    res.json({status:"error", message: "User exists"});
+                }
+            });
+        }else{
+            res.json({status:"error",message:"Use api.easysync.es/URI"});
+        }
     },
     authenticate: function(req, res, next){
-        userModel.findOne({$or:[{email:req.body.email},{username:req.body.username}]}, function(err,userInfo){
-            if(userInfo===null){
-                res.json({status:"error", message: "User or Email not founded"});
-            }else{
-                if(bcrypt.compareSync(req.body.password, userInfo.password)){
-                    const token = jwt.sign({id:userInfo._id},req.app.get('secretKey'), { expiresIn: "1h"});
-                    userModel.updateOne({email:userInfo.email},{$set:{"last_login":new Date()}},function(err){});
-                    res.json({status:"ok",message:"El usuario ha sido autenticado", data:{user:userInfo,token:token}});
+
+        if(checkUri(req.headers.host)){
+            userModel.findOne({$or:[{email:req.body.email},{username:req.body.username}]}, function(err,userInfo){
+                if(userInfo===null){
+                    res.json({status:"error", message: "User or Email not founded"});
                 }else{
-                    res.json({status:"error", message: "Invalid email/user or password"});
+                    if(bcrypt.compareSync(req.body.password, userInfo.password)){
+                        const token = jwt.sign({id:userInfo._id},req.app.get('secretKey'), { expiresIn: "1h"});
+                        userModel.updateOne({email:userInfo.email},{$set:{"last_login":new Date()}},function(err){});
+                        res.json({status:"ok",message:"El usuario ha sido autenticado", data:{user:userInfo,token:token}});
+                    }else{
+                        res.json({status:"error", message: "Invalid email/user or password"});
+                    }
                 }
-            }
-        });
+            });
+        }else{
+            res.json({status:"error",message:"Use api.easysync.es/URI"});
+        }
+        
     },
     delete: function(req,res,next){
-        userModel.deleteOne({_id:req.params.id},function(err){
-            if(err){
-               res.json({status:"error",message:"Problem removing user"});
-            }else{
-                res.json({status:"ok",message:"User removed"});
-            }
-        });
+        
+        if(checkUri(req.headers.host)){
+            userModel.deleteOne({_id:req.params.id},function(err){
+                if(err){
+                   res.json({status:"error",message:"Problem removing user"});
+                }else{
+                    res.json({status:"ok",message:"User removed"});
+                }
+            });
+        }else{
+            res.json({status:"error",message:"Use api.easysync.es/URI"});
+        }
     }
 }
--- backend/API/server.js
+++ backend/API/server.js
@@ -1,22 +1,41 @@
 const express = require('express');
+const subdomain = require('express-subdomain');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const app = express();
 const mongoose = require('./config/database');
+const router = express.Router();
+//const https = require('https');
+//const fs = require('fs');
 var jwt = require('jsonwebtoken');
+const PORT = 80;
+
+/*
+const keys = {
+    key: fs.readFileSync('/root/EasySync/EasySync/keys/server/key_private.pem'),
+    cert: fs.readFileSync('/root/EasySync/EasySync/keys/server/certificade.pem')
+};
+*/
 
 app.set('secretKey','test');
 mongoose.connection.on('error', console.error.bind(console, 'Error de conexion en MongoDB'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/users',users);
+
+app.listen(PORT,function(){
+    console.log("API Listening on api.easysync.es:"+PORT);
+})
+
 app.get('/',function(req,res){
-    res.json({"Developing":"Bulding API REST NodeJS"});
+    if(req.headers.host.includes('api')){
+        res.redirect(307,'/documentation');
+        //res.json({"Developing":"Bulding API REST NodeJS"});
+    }else{
+        res.redirect(307,'/home');
+        //res.json({"Developing":"Main Page"});
+    }
 });
 
-app.use('/api/users',users);
-
-app.listen(3000, function(){
-    console.log('Listening on http://localhost:3000');
-});
