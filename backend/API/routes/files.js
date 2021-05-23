0c1e01f13afb9102a72865a23c1b2d5bada14c94
amolina19 <alejandro.molina.daw@gmail.com>
2021-05-23 22:13:03 +0200

    Api actualizada.

--- backend/API/routes/files.js
+++ backend/API/routes/files.js
@@ -6,6 +6,7 @@ router.post('/storage/upload',filesController.upload);
 router.post('/storage/rename',filesController.renameFile);
 router.post('/storage/delete',filesController.removeFile);
 router.post('/storage/createfolder',filesController.createFolder);
+router.post('/storage/move',filesController.move);
 router.get('/storage/download',filesController.downloadByUrl);
 router.get('/userfiles/',filesController.getUserFiles);
 router.get('/storagesize',filesController.getUserSizeStorage);
